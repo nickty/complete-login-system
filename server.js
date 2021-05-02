2e1205f11a37688435114baeec797310bac7bcb5
Mizanur Rahman <nickty.86@gmail.com>
2021-05-02 15:50:25 +0600

    shaadi initial

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ const express = require("express");
 const mongoose = require("mongoose");
 const bodyParser = require("body-parser");
 const app = express();
+const path = require('path');
 
 const passport = require("passport");
 const users = require("./routes/api/users");
@@ -31,6 +32,14 @@ app.use(passport.initialize());
 require("./config/passport")(passport);
 // Routes
 app.use("/api/users", users);
+
+//serve static asset if we are in product 
+if(process.env.NODE_ENV === 'production'){
+  app.use(express.static('client/build')); 
+  app.get('*', (req, res) => {
+    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+  })
+}
 
   
 const port = process.env.PORT || 5000; // process.env.port is Heroku's port if you choose to deploy the app there
