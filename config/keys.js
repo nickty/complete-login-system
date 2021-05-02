fe1ffd4e87689351d01a269485f6845bd079e6b5
Mizanur Rahman <nickty.86@gmail.com>
2021-05-02 15:40:19 +0600

    full login system

---
+++ config/keys.js
@@ -0,0 +1,12 @@
+// MONGO_DB_LOCAL = mongodb://localhost:27017/shaadi
+// MONGO_ONLINE = mongodb+srv://shaadi:S124578@cluster0.oudmh.mongodb.net/myFirstDatabase?retryWrites=true&w=majority
+// JWT_SECRET = slkdfsldkfjsldkf
+// JWT_EXPIRES_TIME = 7d
+
+// // PORT = 4000
+// NODE_ENV = DEVELOPMENT
+
+module.exports = {
+    mongoURI: "mongodb+srv://shaadi:S124578@cluster0.oudmh.mongodb.net/myFirstDatabase?retryWrites=true&w=majority",
+    secretOrKey: "sdeeRRRR"
+}
